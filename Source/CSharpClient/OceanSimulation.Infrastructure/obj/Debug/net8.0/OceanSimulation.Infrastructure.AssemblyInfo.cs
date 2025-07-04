e1016569dbdadcaaefec94665331639676462a92
beilsm <indexbeilsm@gmail.com>
2025-07-04 17:23:25 +0800

    前端更新

--- Source/CSharpClient/OceanSimulation.Infrastructure/obj/Debug/net8.0/OceanSimulation.Infrastructure.AssemblyInfo.cs
+++ Source/CSharpClient/OceanSimulation.Infrastructure/obj/Debug/net8.0/OceanSimulation.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2025 OceanSim Research Group")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2385519c7fa17d376525687f34e39b9a1c33150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a066ad783fd584ee20bf9ceb6971434d1a30132")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ocean Current Simulation System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ocean Simulation Infrastructure Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
